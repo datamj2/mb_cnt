5ff40912f669e1b35ae5468911b29f2feecb9aaa
datamj2 <mjansen2@gmail.com>
2017-11-13 17:09:20 -0800

    s
    s

--- mb_cnt/AppDelegate.swift
+++ mb_cnt/AppDelegate.swift
@@ -1,4 +1,4 @@
-//
+	//
 //  AppDelegate.swift
 //  mb_cnt
 //
@@ -10,12 +10,47 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-
+    
+    
+    @IBOutlet weak var theLabel: NSTextField!
     @IBOutlet weak var window: NSWindow!
-
+    @IBOutlet weak var theButton: NSButton!
+    
+    var buttonPresses = 0;
+    
+    var statusBar = NSStatusBar.systemStatusBar()
+    var statusBarItem : NSStatusItem = NSStatusItem()
+    var menu: NSMenu = NSMenu()
+    var menuItem : NSMenuItem = NSMenuItem()
+    
+    override func awakeFromNib() {
+        theLabel.stringValue = "You've pressed the button \n \(buttonPresses) times!"
+        
+        //Add statusBarItem
+        statusBarItem = statusBar.statusItemWithLength(-1)
+        statusBarItem.menu = menu
+        statusBarItem.title = "Presses"
+        
+        //Add menuItem to menu
+        menuItem.title = "Clicked"
+        menuItem.action = Selector("setWindowVisible:")
+        menuItem.keyEquivalent = ""
+        menu.addItem(menuItem)
+    }
 
     func applicationDidFinishLaunching(aNotification: NSNotification) {
-        // Insert code here to initialize your application
+        self.window!.orderOut(self)
+           }
+    
+    @IBAction func buttonPressed(sender: NSButton) {
+        buttonPresses+=1
+        theLabel.stringValue = "You've pressed the button \n \(buttonPresses) times!"
+        menuItem.title = "Clicked \(buttonPresses)"
+        statusBarItem.title = "Presses \(buttonPresses)"
+    }
+    
+    func setWindowVisible(sender: AnyObject){
+        self.window!.orderFront(self)
     }
 
     func applicationWillTerminate(aNotification: NSNotification) {
